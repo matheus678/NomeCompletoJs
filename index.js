ac04c28973dbdf8ffb2d1fc3c6d959f6002ea247
= <=>
2020-10-11 20:41:44 -0300

    alterações

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Digite seu primeiro nome:");
+let sobrenome = prompt ("Digite seu sobrenome:");
+let sobrenomeM = sobrenome.toUpperCase();
+
+alert (`Nome completo: ${nome} ${sobrenome}.`);
+alert (`Nome de catálogo: ${sobrenomeM}, ${nome}.`);
